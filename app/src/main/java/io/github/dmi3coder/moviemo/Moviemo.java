c1bd860c102c8da63ae37687ff0ed20e870bf92c
dim3coder <dmi3coder@gmail.com>
2016-12-27 11:34:50 +0200

    Application class Internet permission

---
+++ app/src/main/java/io/github/dmi3coder/moviemo/Moviemo.java
@@ -0,0 +1,13 @@
+package io.github.dmi3coder.moviemo;
+
+import android.app.Application;
+
+public class Moviemo extends Application {
+    public static final String API_KEY = "d713f705e425274fbb576076c15be337";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
