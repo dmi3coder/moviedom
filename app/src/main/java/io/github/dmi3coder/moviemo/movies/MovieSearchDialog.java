979667bc87418ec46796f1d1de37cc656872de63
dim3coder <dmi3coder@gmail.com>
2016-12-29 20:02:13 +0200

    Empty Search dialog

---
+++ app/src/main/java/io/github/dmi3coder/moviemo/movies/MovieSearchDialog.java
@@ -0,0 +1,22 @@
+package io.github.dmi3coder.moviemo.movies;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.view.ViewGroup;
+
+import io.github.dmi3coder.moviemo.R;
+
+public class MovieSearchDialog extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = new Dialog(getContext(), R.style.FullScreenDialog);
+        dialog.setContentView(R.layout.dialog_search);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        return dialog;
+    }
+}
