da8cbba227bcf2f43a2c65c55563dcddddbed999
dim3coder <dmi3coder@gmail.com>
2016-12-29 19:58:20 +0200

    Fab unhandled

--- app/src/main/java/io/github/dmi3coder/moviemo/movies/MovieActivity.java
+++ app/src/main/java/io/github/dmi3coder/moviemo/movies/MovieActivity.java
@@ -1,17 +1,27 @@
 package io.github.dmi3coder.moviemo.movies;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import io.github.dmi3coder.moviemo.R;
 
 public class MovieActivity extends AppCompatActivity {
     MoviePresenter presenter;
+    FloatingActionButton searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        searchButton = ((FloatingActionButton) findViewById(R.id.fab_search));
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: 12/29/16 add search action
+            }
+        });
         MovieListFragment fragment = new MovieListFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.frame_movie,fragment,"tag").commit();
         presenter = new MoviePresenter(fragment);
